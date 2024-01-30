1a462d6cf5ffe0f4bd9f6870b55a83096a5768cf
AndrisTolmanis <andris.tolmanis@gmail.com>
2024-01-30 13:23:54 +0000

    Added more stolen code.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import serial
 import glob
 import sys
 import os
@@ -6,7 +7,6 @@ import os
 while True:
   now = time.strftime("%H:%M:%S")
   print("Hi {}".format(now))
-  time.sleep(1)
 
   if sys.platform.startswith('win'):
     ports = ['COM%s' % (i + 1) for i in range(256)]
@@ -19,3 +19,14 @@ while True:
     raise EnvironmentError('Unsupported platform')
 
   print(ports)
+  result = []
+  for port in ports:
+    try:
+      s = serial.Serial(port)
+      s.close()
+      result.append(port)
+    except (OSError, serial.SerialException):
+      pass
+
+  print(result)
+  time.sleep(1)
